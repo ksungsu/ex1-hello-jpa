50346767ad6866520dca0540883b815c0eae8528
ksungsu <zxzxzlxlzlxl@naver.com>
2021-12-25 14:14:13 +0900

    TIL 62th day

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,42 +1,36 @@
 package hellojpa;
 
 import javax.persistence.*;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Date;
 
 @Entity //JPA가 관리하는 객체
+@TableGenerator(name="MEMBER_SEQ_GENERATOR",
+        table = "MY_SEQUENCES",
+        pkColumnValue = "MEMBER_SEQ", allocationSize = 1)
 public class Member {
 
     @Id //DB PK와 연결
+    @GeneratedValue(strategy = GenerationType.TABLE, generator = "MEMBER_SEQ_GENERATOR")
     private Long id;
 
-    @Column(name = "name")
+    @Column(name = "name", nullable = false)
     private String username;
-
-    private Integer age;
-
-    //사용자 또는 관리자
-    @Enumerated(EnumType.STRING)
-    private RoleType roleType;
-
-    //생성 일자
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createdDate;
-
-    //수정 일자
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastModifiedDate;
-
-    private LocalDate testLocalDate;
-    private LocalDateTime testLocalDateTime;
-
-    @Lob
-    private String description;
-
 
     public Member() {
     }
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
